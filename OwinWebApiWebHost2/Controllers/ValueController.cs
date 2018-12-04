ebd6d9715e312ccba0f866d030014e5a3eeec2d3
RoyLiu <121407741@qq.com>
2018-12-04 12:31:05 +0800

    add OwinWebApiWebHost without Global.asax

---
+++ OwinWebApiWebHost2/Controllers/ValueController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace OwinWebApiWebHost2.Controllers
+{
+    public class ValueController : ApiController
+    {
+        // GET api/<controller>
+        public string Get()
+        {
+            return "Hello world";
+        }
+    }
+}
---
+++ OwinWebApiWebHost2/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+using OwinWebApiWebHost2;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace OwinWebApiWebHost2
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+
+            app.UseWebApi(config);
+        }
+    }
+}
